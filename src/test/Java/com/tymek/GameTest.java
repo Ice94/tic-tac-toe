17b17d08230beb5e27d1c3805a1e547b249f13dc
Bratek <mhbratek@gmail.com>
2017-07-03 20:54:58 +0200

    Clean up in test directory. Remove unused class.

--- src/test/Java/com/tymek/GameTest.java
+++ src/test/Java/com/tymek/GameTest.java
@@ -3,11 +3,14 @@ package com.tymek;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import static org.testng.Assert.assertEquals;
+
 /**
  * Created by bratek on 03.07.17.
  */
 public class GameTest {
 
+    private static final int NUMBER_OF_PLAYERS = 2;
     private Game game;
 
     @BeforeTest
@@ -15,13 +18,16 @@ public class GameTest {
         game = new HumansGame();
     }
 
-
-
-    // TODO: Just to not forget about the this test.
     @Test
-    public void testCreatingPlayers(){
+    public void createTwoPlayersWithGivenValues(){
+        game.createPlayers();
 
-
+        assertEquals(getNumberOfPlayers(), NUMBER_OF_PLAYERS);
     }
+
+    private int getNumberOfPlayers() {
+        return game.getPlayers().getPlayers().size();
+    }
+
 
 }
