c36b40463b16354ab322d926756cdd200bbb769d
Mateusz <mhbratek@gmail.com>
2017-07-04 08:41:39 +0200

    Javadoc for Board interface - added.

--- src/main/java/com/tymek/board/Board.java
+++ src/main/java/com/tymek/board/Board.java
@@ -3,11 +3,25 @@ package com.tymek.board;
 import java.util.Map;
 
 /**
- *
+ * Interface of board.
  * @author Mateusz Bratek
  */
 public interface Board {
+    /**
+     * Returns board.
+     * @return map of integers with strings.
+     * */
     Map<Integer, String> getBoard();
+
+    /**
+     * Returns horizontal length of the board.
+     * @return Horizontal length of the board.
+     * */
     int getHorizontalLength();
+
+    /**
+     * Returns vertical length of the board.
+     * @return vertical length of the board.
+     * */
     int getVerticalLength();
 }
