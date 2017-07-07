ca13766f2f355286c4bc2e04f16070a2db22f713
Mateusz <mhbratek@gmail.com>
2017-07-07 15:07:50 +0200

    Later to do at home.

---
+++ src/test/Java/com/tymek/player/PlayersScoreTest.java
@@ -0,0 +1,13 @@
+package com.tymek.player;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+/**
+ * Created by bratek on 07.07.17.
+ */
+@Test
+public class PlayersScoreTest {
+
+}
